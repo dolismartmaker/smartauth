dccbe82627895ed75f5a3916722be1503d4cecc5
Eric Seigne <eric.seigne@cap-rel.fr>
2025-11-03 19:30:51 +0100

    fix sql

--- sql/llx_smartauth_auth.sql
+++ sql/llx_smartauth_auth.sql
@@ -19,16 +19,16 @@ CREATE TABLE llx_smartauth_auth(
     salt varchar(32),
 	date_creation datetime NOT NULL,
 	date_lastused datetime,
-	refresh_count INTEGER DEFAULT 0
+	refresh_count INTEGER DEFAULT 0,
 	date_eol datetime,
 	tms timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	fk_user_creat integer NOT NULL, -- id of created user
 	fk_user_modif integer,
 	fk_authid integer NOT NULL, -- id of authenticated user or element
-	parent_token_id INTEGER DEFAULT NULL AFTER fk_authid;
+	parent_token_id INTEGER DEFAULT NULL,
 	auth_element varchar(128) NOT NULL, -- may be user or societe_account or ...
 	token_type VARCHAR(20) DEFAULT 'access',
 	ip varchar(50) DEFAULT '',
 	status integer NOT NULL,
-	entity integer DEFAULT 1 NOT NULL,
+	entity integer DEFAULT 1 NOT NULL
 ) ENGINE=innodb;
