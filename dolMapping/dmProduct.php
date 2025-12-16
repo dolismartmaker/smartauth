82f04e0d942ce12208b198f586f8fd5ec949d7e8
Eric Seigne <eric.seigne@cap-rel.fr>
2025-12-16 18:46:12 +0100

    mapping product

---
+++ dolMapping/dmProduct.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright (c) 2025 Eric Seigne <eric.seigne@cap-rel.fr>
+ * Copyright (c) 2025 Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace SmartAuth\DolibarrMapping;
+
+require_once DOL_DOCUMENT_ROOT . '/product/class/product.class.php';
+
+class dmProduct extends dmBase
+{
+	use dmTrait;
+
+	protected $type = "object";
+
+	// Dolibarr field => Front field
+	// See documentation/api-naming-convention.md
+	protected $listOfPublishedFields = [
+		'rowid'             => 'id',
+		'ref'               => 'ref',
+		'label'             => 'label',
+		'description'       => 'description',
+		'type'              => 'type',
+		'price'             => 'price_excl_tax',
+		'price_ttc'         => 'price_incl_tax',
+		'price_min'         => 'price_min_excl_tax',
+		'price_min_ttc'     => 'price_min_incl_tax',
+		'price_base_type'   => 'price_base_type',
+		'tva_tx'            => 'vat_rate',
+		'barcode'           => 'barcode',
+		'weight'            => 'weight',
+		'length'            => 'length',
+		'width'             => 'width',
+		'height'            => 'height',
+		'stock'             => 'stock',
+		'seuil_stock_alerte' => 'stock_alert_threshold',
+		'note_public'       => 'public_note',
+		'note_private'      => 'private_note',
+		'datec'             => 'created_at',
+		'tosell'            => 'for_sale',
+		'tobuy'             => 'for_purchase',
+	];
+
+	/**
+	 * object constructor
+	 *
+	 * @return  [type]  [return description]
+	 */
+	public function __construct()
+	{
+		$this->boot();
+	}
+}
