4f04f7fbdfd8840bdcf5624b71b9d9e6502757a2
Eric Seigne <eric.seigne@cap-rel.fr>
2025-12-16 18:46:49 +0100

    mapping civility

---
+++ dolMapping/dmCcivility.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright (c) 2025 Eric Seigne <eric.seigne@cap-rel.fr>
+ * Copyright (c) 2025 Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace SmartAuth\DolibarrMapping;
+
+require_once DOL_DOCUMENT_ROOT . '/core/class/cgenericdic.class.php';
+
+class dmCcivility extends dmBase
+{
+	use dmTrait;
+
+	protected $type = "dict";
+
+	// Dolibarr field => Front field
+	// See documentation/api-naming-convention.md
+	// Dictionaries expose code + label for use in forms and display
+	protected $listOfPublishedFields = [
+		'code'              => 'code',
+		'label'             => 'label',
+	];
+
+	/**
+	 * object constructor
+	 *
+	 * @return  [type]  [return description]
+	 */
+	public function __construct()
+	{
+		$this->boot();
+	}
+}
