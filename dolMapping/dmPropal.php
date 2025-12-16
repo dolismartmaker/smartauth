6ee2568e6e1cea6b66af308b7a19a47b543a6802
Eric Seigne <eric.seigne@cap-rel.fr>
2025-12-16 18:46:01 +0100

    mapping propal

---
+++ dolMapping/dmPropal.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Copyright (c) 2025 Eric Seigne <eric.seigne@cap-rel.fr>
+ * Copyright (c) 2025 Paolo Debaisieux <paolo.debaisieux@cap-rel.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace SmartAuth\DolibarrMapping;
+
+require_once DOL_DOCUMENT_ROOT . '/comm/propal/class/propal.class.php';
+
+class dmPropal extends dmBase
+{
+	use dmTrait;
+
+	protected $type = "object";
+
+	// Dolibarr field => Front field
+	// See documentation/api-naming-convention.md
+	protected $listOfPublishedFields = [
+		'rowid'             => 'id',
+		'ref'               => 'ref',
+		'ref_client'        => 'customer_ref',
+		'datec'             => 'created_at',
+		'date'              => 'date_proposal',
+		'date_valid'        => 'validated_at',
+		'fin_validite'      => 'date_expiry',
+		'delivery_date'     => 'date_delivery',
+		'fk_soc'            => 'customer',
+		'fk_projet'         => 'project',
+		'total_ht'          => 'total_excl_tax',
+		'total_tva'         => 'total_vat',
+		'total_ttc'         => 'total_incl_tax',
+		'note_public'       => 'public_note',
+		'note_private'      => 'private_note',
+		'statut'            => 'status',
+	];
+
+	/**
+	 * object constructor
+	 *
+	 * @return  [type]  [return description]
+	 */
+	public function __construct()
+	{
+		$this->boot();
+	}
+}
